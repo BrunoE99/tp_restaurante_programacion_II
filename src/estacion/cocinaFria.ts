c05af28a47cad3290e71fbdb58c7a93931bf1fcd
buenofior <buenofiorellam@gmail.com>
2026-09-21 14:55:50 -0300

    agrego clase estacion y sus subclases: parrilla,cocina fria,cocina dulce y barra

---
+++ src/estacion/cocinaFria.ts
@@ -0,0 +1,6 @@
+import  Estacion  from "./estacion";
+export default class CocinaFria extends Estacion{
+
+
+    
+}
---
+++ src/estacion/estacion.ts
@@ -0,0 +1,8 @@
+export default abstract class Estacion{
+
+public preparar(item:any):any{
+    return item;
+}
+
+
+}
