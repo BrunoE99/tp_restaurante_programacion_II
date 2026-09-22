46a8ce778ba82174c5a451d1cad7269931a232b0
Bruno Enrigo <brunoenrigo@gmail.com>
2026-09-21 23:32:25 -0300

    modifique cosas chiquitas de pedido y gestor

--- src/pedido/gestor_pedidos.ts
+++ src/pedido/gestor_pedidos.ts
@@ -1,3 +1,4 @@
+import { ESTADO_PEDIDO } from "../enums/estado_pedido";
 import { Pedido } from "./pedido";
 
 export class GestorPedidos {
@@ -20,6 +21,42 @@ export class GestorPedidos {
 
     public pedidoPago(medioPago: mediosDePago): void {
         this.pedido.setMedioDePago(medioPago);
+    }
+
+    public consultarHistorial(numPedido: number): void {
+        // usar la funcion del MAP para validar el Pedido
+    }
+
+    public agregarItem(item: Item): void {
+        this.pedido.agregarItem(item);
+    }
+
+    public eliminarItem(item: Item): void {
+        this.pedido.eliminarItem(item);
+    }
+
+    public agregarCombo(combo: Combo): void {
+        this.pedido.agregarCombo(combo);
+    }
+
+    public eliminarCombo(combo: Combo): void {
+        this.pedido.eliminarCombo(combo);
+    }
+
+    public modificarPedido(): void {
+        // que debia hacer esto?
+    }
+
+    public confirmarPedido(): void {
+        this.pedido.setEstadoPedido(ESTADO_PEDIDO.EN_PREPARACION);
+    }
+
+    public asignarAEstaciones(): void {
+        // envia cada item a su estacion designada. probablemente llama a dentro de Pedido.
+    }
+
+    public calcularPrecioTotal(): number {
+        // calcula el precio con todos los items+combos y suma los descuentos?
     }
 
 }
--- src/pedido/pedido.ts
+++ src/pedido/pedido.ts
@@ -6,8 +6,8 @@ export abstract class Pedido {
 
     private numeroDePedido: number;
     private cliente: Cliente;
-    private items: Item[];
-    private combos: Combo[];
+    private items: Item[]; // lista enlazada
+    private combos: Combo[]; // lista enlazada
     private precioTotal: number;
     private medioDePago: MediosDePago;
     private estadoDelPedido: ESTADO_PEDIDO;
@@ -45,6 +45,10 @@ export abstract class Pedido {
         return this.estadoDelPedido;
     }
 
+    public setEstadoPedido(estado: ESTADO_PEDIDO): void {
+        this.estadoDelPedido = estado;
+    }
+
     private calculoDiaActual(): DIA_SEMANA {
         const fecha = new Date();
         const numeroDia = fecha.getDay();
@@ -54,5 +58,28 @@ export abstract class Pedido {
     protected abstract pedir(): void;
 
     protected abstract entregar(): void;
+
+    public estadoItems(): ESTADO_ITEM {
+        // verificar lista enlazada de Items/Combos para verificar el estado
+        return ESTADO_ITEM.EN_PREPARACION;
+    }
+
+    public agregarItem(item: Item): void {
+        // tiene que cambiarse a lista enlazada
+        this.items.push(item);
+    }
+
+    public eliminarItem(item: Item): void {
+        // eliminar de la lista
+    }
+
+    public agregarCombo(combo: Combo): void {
+        // tiene que cambiarse a lista enlazada
+        this.combos.push(combo);
+    }
+
+    public eliminarCombo(combo: Combo): void {
+        // eliminar de la lista
+    }
 
 }
